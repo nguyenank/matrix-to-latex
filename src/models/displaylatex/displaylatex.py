fdda9f5d3c054ec2283ad322e771df49a4a6064d
An <anknguyenanknguyen@gmail.com>
2020-11-03 17:17:08 -0600

    edit displaylatex.py in accordance with pylint

--- src/models/displaylatex/displaylatex.py
+++ src/models/displaylatex/displaylatex.py
@@ -1,17 +1,18 @@
-# displaylatex.py
-#
-# takes in a string of LaTeX s, and a filepath (with no extension), and outputs
-# a rendered pdf version of the LaTeX to the filepath + .pdf
-#
-# TO USE: run 'resultstolatex(s, filepath)'
+"""
+displaylatex.py
 
-from pylatex import Document, Command, NoEscape
+takes in a string of LaTeX s, and a filepath (with no extension), and outputs
+a rendered pdf version of the LaTeX to the filepath + .pdf
+
+TO USE: run 'resultstolatex(s, filepath)'
+"""
+
 import os
+from pylatex import Document, Command, NoEscape
 
-
-def displaylatex(s, filepath):
+def displaylatex(latexstring, filepath):
     """
-        takes in a string of LaTeX s, and a filepath (with no extension), and outputs
+        takes in a string of LaTeX latexstring, and a filepath (with no extension), and outputs
         a rendered pdf version of the LaTeX to the filepath + .pdf
     """
     doc = Document('basic', page_numbers=False, font_size='LARGE')
@@ -23,7 +24,7 @@ def displaylatex(s, filepath):
             r'\geometry{paperwidth=2.5in,paperheight=2.5in, margin=0.1in, hoffset=-0.2in}'
         ))
     # NoEscape renders \'s properly instead of converting to \backslash
-    doc.append(NoEscape(s))
+    doc.append(NoEscape(latexstring))
     doc.generate_pdf(filepath, clean_tex=False)
     # remove generated tex file
     os.system(f'rm {filepath+".tex"}')
