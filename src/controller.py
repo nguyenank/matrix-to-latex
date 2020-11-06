32844f0bfbd3845670af777c76afd3597fb78eb0
Maya <mabodominguez@hmc.edu>
2020-11-05 21:44:02 -0500

    fix post request between confirm_image and loading

--- src/controller.py
+++ src/controller.py
@@ -48,14 +48,14 @@ def upload_files1():
 
 @app.route('/loading', methods=['POST'])
 def predict():
-
+    filename = request.form['filename']
+    file_root, file_ext = os.path.splitext(filename)
     # run YOLOv5 model
     os.system(f'python models/yolov5/detect.py ' \
             f'--weights models/yolov5/best-2.pt --source {app.config["STATIC_MATRIX_FOLDER"]} ' \
             f'--out {app.config["TEMP_FOLDER"]} --img 416 --conf 0.4 --save-txt')
     # run toLatex model
-    latex = results_to_latex(
-        os.path.join(app.config['TEMP_PATH'], file_root + '.txt'), CLASSES)
+    latex = results_to_latex(os.path.join(app.config['TEMP_PATH'], file_root + '.txt'), CLASSES)
     latex_filename = os.path.join(app.config['STATIC_MATRIX_PATH'],
                                   file_root)
     # run renderLatex model
@@ -63,23 +63,6 @@ def predict():
     # delete temporary folder
     os.system('rm -r temp')
     return render_template('results.html', latex=latex, matrix_image = full_filename, image_filename= filename,latex_pdf = latex_filename+'.pdf')
-
-# def reupload():
-#     os.system(f'rm -r {app.config["STATIC_MATRIX_FOLDER"]}')
-#     os.system(f'mkdir {app.config["STATIC_MATRIX_FOLDER"]}')
-#     uploaded_file = request.files['file']
-#     filename = uploaded_file.filename
-#     if filename != '':
-#         # only continue with nonempty filename
-#         file_root, file_ext = os.path.splitext(filename)
-#         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-#             # not a valid file extension
-#             abort(400)
-#         # save the uplaoded file to STATIC_PATH
-#         full_filename = os.path.join(app.config['STATIC_MATRIX_PATH'],
-#                                      filename)
-#         uploaded_file.save(full_filename)
-#         return render_template('confirm_image.html', image_filename = filename, matrix_image = full_filename)
 
 
 @app.route('/about')
